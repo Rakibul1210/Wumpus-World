e41a7e8d814439aaf701ea9a1c763cde108f41db
RakibulIslamIIT <bsse1210@iit.du.ac.bd>
2023-09-22 16:40:33 +0600

    Agent Class Added

--- Backend/server.js
+++ Backend/server.js
@@ -6,24 +6,27 @@ const numberOfPits = 20; // Specify the number of pits
 const numberOfGold = 3; // Specify the number of gold items
 const numberOfWumpus = 2; // Specify whether the Wumpus is present
 const board = new Board(gridSize, numberOfPits, numberOfGold, numberOfWumpus);
+
+const Agent = new Agent(0, 0);
+
 board.display();
 
 
-console.log("Moving Right");
-board.moveRight();
-board.display();
+// console.log("Moving Right");
+// board.moveRight();
+// board.display();
 
-console.log("Moving Down");
-board.moveDown();
-board.display();
+// console.log("Moving Down");
+// board.moveDown();
+// board.display();
 
-console.log("Moving Left");
-board.moveLeft();
-board.display();
+// console.log("Moving Left");
+// board.moveLeft();
+// board.display();
 
-console.log("Moving Up");
-board.moveUp();
-board.display();
+// console.log("Moving Up");
+// board.moveUp();
+// board.display();
 
 
 
